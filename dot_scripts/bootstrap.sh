02fa04805746ec35807431222de9beea75389797
Morgan Wattiez <morgan.wattiez@collibra.com>
2019-11-09 15:31:15 +0100

    fix spotify alias + add baton spotify cli

--- dot_scripts/bootstrap.sh
+++ dot_scripts/bootstrap.sh
@@ -148,6 +148,23 @@ pip install -r requirements.txt
 # pip3 install pipenv
 # pyenv install 3.6.0
 
+# Install baton (CLI to manage Spotify playback) https://github.com/joshuathompson/baton
+case "$(uname -s)" in
+   Darwin)
+     echo 'Mac OS X'
+     curl -sSL https://github.com/joshuathompson/baton/releases/download/0.1.7/baton-0.1.7-darwin-amd64 -o /usr/local/bin/baton && chmod +x /usr/local/bin/baton
+     ;;
+
+   Linux)
+     echo 'Linux'
+    curl -sSL https://github.com/joshuathompson/baton/releases/download/0.1.7/baton-0.1.7-linux-amd64 -o /usr/local/bin/baton && chmod +x /usr/local/bin/baton
+     ;;
+   *)
+     echo 'Non supported OS'
+     exit
+     ;;
+esac
+
 # Upgrade
 ~/.scripts/upgrade.sh
 
