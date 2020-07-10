c69bad4acf6a2fa7068f65aceb44eb10fce14338
MorganGeek <wattiez.morgan@gmail.com>
2020-07-10 13:27:47 +0200

    aliases for terragrunt

---
+++ dot_aliases/terragrunt.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+alias tg='terragrunt'
+alias tga='terragrunt apply'
+alias tgc='terragrunt console'
+alias tgd='terragrunt destroy'
+alias tgf='terragrunt fmt'
+alias tgg='terragrunt graph'
+alias tgim='terragrunt import'
+alias tgin='terragrunt init'
+alias tgo='terragrunt output'
+alias tgp='terragrunt plan'
+alias tgpr='terragrunt providers'
+alias tgr='terragrunt refresh'
+alias tgsh='terragrunt show'
+alias tgt='terragrunt taint'
+alias tgut='terragrunt untaint'
+alias tgv='terragrunt validate'
+alias tgw='terragrunt workspace'
+alias tgs='terragrunt state'
+alias tgfu='terragrunt force-unlock'
+alias tgwst='terragrunt workspace select'
+alias tgwsw='terragrunt workspace show'
+alias tgssw='terragrunt state show'
+alias tgwde='terragrunt workspace delete'
+alias tgwls='terragrunt workspace list'
+alias tgsls='terragrunt state list'
+alias tgwnw='terragrunt workspace new'
+alias tgsmv='terragrunt state mv'
+alias tgspl='terragrunt state pull'
+alias tgsph='terragrunt state push'
+alias tgsrm='terragrunt state rm'
+alias tgay='terragrunt apply -auto-approve'
+alias tgdy='terragrunt destroy -auto-approve'
+alias tginu='terragrunt init -upgrade'
+alias tgpde='terragrunt plan --destroy'
