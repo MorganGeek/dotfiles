c415f3554382faa5c9912e237b2aa63db7f15b91
MorganGeek <wattiez.morgan@gmail.com>
2020-07-13 16:02:47 +0200

    fix: lynx needed a openssl+rvm upgrade
    - add alias for gpg2 to point to gpg, needed for rvm install script
    - reinstall openssl (brew reinstall openssl) to fix lynx
    - install rvm

--- dot_aliases/secure.sh
+++ dot_aliases/secure.sh
@@ -3,6 +3,7 @@
 alias mgmt='ssh $(whoami)@$PROXY_HOST -i $HOME/.ssh/id_ed25519'
 alias m='mgmt'
 alias tunnel='ssh -D 8000 -N $(whoami)@$PROXY_HOST -i $HOME/.ssh/id_ed25519'
+alias gpg2="gpg"
 alias sshpub='cat $HOME/.ssh/id_ed25519.pub'
 alias ssla="openssl x509 -alias -noout -in" # Usage : ssla <cert>
 alias encrypt='ansible-vault encrypt **/vault.yml --vault-password-file=$VAULT_PASSWORD_FILE'
--- dot_scripts/rubydeps.sh
+++ dot_scripts/rubydeps.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
-case "$(uname -s)" in
-    Linux)
-        echo '(Linux) Installing ruby'
-        curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
-        curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
-        curl -L get.rvm.io | bash -s stable
-        source /etc/profile.d/rvm.sh
-        rvm reload
-        rvm requirements run
-        rvm install 2.3.7
-        ;;
-esac
+echo '(Linux) Installing ruby'
+curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
+curl -L get.rvm.io | bash -s stable
+source /etc/profile.d/rvm.sh
+rvm reload
+rvm requirements run
+rvm install 2.3.7
