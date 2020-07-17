159ac666799565529840846a59fa56b3fcbcb707
MorganGeek <wattiez.morgan@gmail.com>
2020-07-17 14:46:18 +0200

    improve aliases portability

--- dot_aliases/misc.sh
+++ dot_aliases/misc.sh
@@ -20,10 +20,11 @@ alias current_time='current_hour'
 alias meditate='browse chrome-extension://onjcfgnjjbnflacfbnjaapcbiecckilk/popup.html#fullscreen'
 alias runhooks='pre-commit run -a'
 # https://unix.stackexchange.com/a/205854/220566
-alias trim="awk '{\$1=\$1};1'"
+alias trim='awk "{\$1=\$1};1"'
+alias trem="awk '{\$1=\$1};1'"
 # via https://github.com/richinfante/dotfiles/blob/0e1df2d833257e3a3ca2e4fcc8f2f128d52b5d29/dotfiles/.profile_setup/30-aliases.sh
-alias lowercase='tr "[:upper:]" "[:lower:]"'
-alias uppercase='tr "[:lower:]" "[:upper:]"'
+alias lowercase='tr "[[:upper:]]" "[[:lower:]]"'
+alias uppercase='tr "[[:lower:]]" "[[:upper:]]"'
 alias to_lower='lowercase'
 alias to_upper='uppercase'
 alias tryhook='pre-commit try-repo .'
